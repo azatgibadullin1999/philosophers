449e62ce7b91773fc6a2bc71ceb6366cd3e460a3
root <root@DESKTOP-2MJ007O.localdomain>
2021-07-09 21:29:34 +0300

    remade condition number of argument

--- philo/main/philo_main.c
+++ philo/main/philo_main.c
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 19:11:28 by root              #+#    #+#             */
-/*   Updated: 2021/07/09 00:00:14 by root             ###   ########.fr       */
+/*   Updated: 2021/07/09 21:07:29 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ int	main(int argc, char **argv)
 	t_common_structs	init;
 	t_philo				*philo;
 
-	// if (argc != 5)
-		// return (ERROR);
+	if (argc < 5 || argc > 6)
+		return (ERROR);
 	if (prj_philosophers_init(argc, argv, &philo, &init))
 		return (ERROR);
 	philo_pthreads(philo);
